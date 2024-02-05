5d58cad7c2e4c0fe336e39adce8277572d89733e
Si Dao <dao@singoc.com>
2024-02-05 14:56:25 +0700

    Adding Noice for better UX

--- init.lua
+++ init.lua
@@ -155,6 +155,22 @@ function _install_packages()
         },
         { 'vimwiki/vimwiki' },
         { 'stsewd/sphinx.nvim' },
+
+        {
+            "folke/noice.nvim",
+            event = "VeryLazy",
+            opts = {
+                -- add any options here
+            },
+            dependencies = {
+                -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+                "MunifTanjim/nui.nvim",
+                -- OPTIONAL:
+                --   `nvim-notify` is only needed, if you want to use the notification view.
+                --   If not available, we use `mini` as the fallback
+                "rcarriga/nvim-notify",
+            }
+        }
     }
     require("lazy").setup(plugins, opts)
 end
